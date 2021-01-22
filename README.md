65695f1633ccbea321bfdc85fefb4a598862c838
RedBullet2000 <63525348+RedBullet2000@users.noreply.github.com>
2021-01-22 18:15:52 +0900

    update: モジュールのクラス化

---
+++ README.md
@@ -0,0 +1,2 @@
+## Line Notify API Document
+https://notify-bot.line.me/doc/ja/
--- main.py
+++ main.py
@@ -3,24 +3,36 @@ import requests
 import env
 
 LINE_API_URL = 'https://notify-api.line.me/api/notify'
-TOKEN = env.TOKEN
 
-def _get_headers(token):
-    return {'Authorization': f'Bearer {token}'}
+class NoTokenError(Exception):
+    pass
 
-def send_massage(content: str):
-    r = requests.post(LINE_API_URL, headers=_get_headers(TOKEN), data={'message': {content}})
-    return r.status_code
+class Client(object):
+    def __init__(self, token=None):
+        self.token = token
 
-def send_massage_and_image(content: str, image_path: str):
-    binary_image = open(image_path, mode='rb')
-    image = {'imageFile': binary_image}
-    r = requests.post(LINE_API_URL, headers=_get_headers(TOKEN), data={'message': {content}}, files=image)
-    return r.status_code
+    def set_token(self, token):
+        self.token = token
+        return self
+
+    def _get_headers(self):
+        if self.token is not None:
+            return {'Authorization': f'Bearer {self.token}'}
+        else:
+            raise NoTokenError(f'Line notify token is not set.')
+
+    def send_massage(self, content: str):
+        if len(content) <= 1000:
+            r = requests.post(LINE_API_URL, headers=self._get_headers(), data={'message': {content}})
+
+    def send_massage_and_image(self, content: str, image_path: str):
+        binary_image = open(image_path, mode='rb')
+        image = {'imageFile': binary_image}
+        r = requests.post(LINE_API_URL, headers=self._get_headers(), data={'message': {content}}, files=image)
 
 def main():
-    send_massage('This is test!')
-    send_massage_and_image('image', './test/test.png')
+    client = Client(env.TOKEN)
+    client.send_massage('test')
 
 if __name__ == '__main__':
     main()
